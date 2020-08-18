6339f9179d7e18ee79db360972c012790a7e0827
hdm08 <65184650+hdm08@users.noreply.github.com>
2020-08-18 18:33:44 +0530

    Add files via upload

---
+++ array-left-rotation.py
@@ -0,0 +1,17 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n,d = map(int,input().split())
+
+    a = list(map(int, input().rstrip().split()))
+    b=a[0:d]
+    for i in range(d):
+        x=a.pop(0)
+    c=a+b
+    print(" ".join(map(str,c)))
